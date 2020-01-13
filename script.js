2142b82e9cfdb04fa45f94f0baa2264caca9993a
David Ortiz <5839911+daviti@users.noreply.github.com>
2020-01-12 20:07:08 -0800

    dice game and variable
    game of dice

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-/*****************************
-* Variables and data types
-*/
-/*
+
 var firstName = 'John';
 console.log(firstName);
 
@@ -21,7 +18,7 @@ console.log(job);
 var _3years = 3;
 var johnMark = 'John and MArk';
 var if = 23;
-*/
+
 
 
 
